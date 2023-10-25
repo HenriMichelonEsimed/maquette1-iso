5a234d420fc2d2a2ba1257e9adf0e1424ac180c2
HenriMichelon <hmichelon@mailcourrier.fr>
2023-10-25 17:46:34 +0200

    cleanup

--- zones/PM/pm_1.gd
+++
@@ -1,7 +0,0 @@
-extends Zone
-
-func _on_orange_button_using(_is_used):
-	$Stair/DistantDoor.use()
-
-func _on_orange_button_2_using(_is_used):
-	GameState.events_queue.postEvent("pm_pm_2", "DistantDoor", "use")
