fd73545100e79ef182901f6f263a3bd7d71bff90
HenriMichelon <hmichelon@mailcourrier.fr>
2023-11-05 21:24:42 +0100

    level0

--- props/architecture/walls/wall_2_ext_light.gd
+++
@@ -1,6 +0,0 @@
-extends StaticBody3D
-
-@export var light_color:Color = Color("e6c42e")
-
-func _ready():
-	$omni_light_bar.set_color(light_color)
--- props/lights/omni_light_bar.gd
+++ props/lights/omni_light_bar.gd
@@ -1,5 +1,10 @@
 extends Node3D
 
+@export var color:Color = Color(Color.WHITE)
+
+func _ready():
+	set_color(color)
+
 func set_color(new_color:Color):
 	$Support/Neon/Light/OmniLight3D.light_color = new_color
 	$Support/Neon/Light.material = StandardMaterial3D.new()
