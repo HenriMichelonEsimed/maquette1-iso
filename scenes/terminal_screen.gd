391336e0745fcbf9cf104f02b27d037a8a512967
HenriMichelon <hmichelon@mailcourrier.fr>
2023-11-17 13:37:51 +0100

    mesages

--- scenes/terminal_screen.gd
+++ scenes/terminal_screen.gd
@@ -8,7 +8,14 @@ func _on_button_back_pressed():
 
 func _on_button_list_messages_pressed():
 	$Screen/Content/Control/ListMessages.visible = true
+	$Screen/Content/Control/LabelMessage.visible = false
 
 
 func _on_button_home_term_pressed():
 	$Screen/Content/Control/ListMessages.visible = false
+	$Screen/Content/Control/LabelMessage.visible = false
+
+
+func _on_list_messages_item_selected(index):
+	$Screen/Content/Control/ListMessages.visible = false
+	$Screen/Content/Control/LabelMessage.visible = true
