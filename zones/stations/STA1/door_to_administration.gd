afb3c37d57a2dfa6941dbd14ad3bfa18e698f60e
HenriMichelon <hmichelon@mailcourrier.fr>
2023-11-20 10:27:23 +0100

    level0

---
+++ zones/stations/STA1/door_to_administration.gd
@@ -0,0 +1,6 @@
+extends Usable
+
+func _check_use() -> bool:
+	var check = GameState.main_quest.current.key > "QAMain0"
+	if not check: NotifManager.notif("You need to read your messages")
+	return check
