e02e68d982d87853ca7459a958f0a303dc72b801
HenriMichelon <148531229+HenriMichelon@users.noreply.github.com>
2023-11-24 22:13:38 +0100

    trade

--- lib/nodes/interactive_character.gd
+++ lib/nodes/interactive_character.gd
@@ -8,13 +8,29 @@ signal end_talk()
 
 var discussion:Array
 var current:Array
+var items_list:Array
+var items:ItemsCollection
+var generate_chance:int
 
-func _init(disc = ["Hello !", [["Bye.", _end]] ]):
+func _init(disc = ["Hello !", [["Bye.", _end]] ], it = [], gen = 0):
 	discussion = disc
+	items_list = it
+	if (not items_list.is_empty()):
+		items = ItemsCollection.new()
+		generate_chance = gen
 
 func _ready():
 	set_collision_layer_value(5, true)
-	
+	var rng = RandomNumberGenerator.new()
+	if (items != null) and ((rng.randi_range(0, generate_chance) == 1) or (items.count() == 0)):
+		for item in items_list:
+				var max_qty = 1
+				if (item.size() == 3):
+					max_qty = item[2]
+				var qty = rng.randi_range(0, max_qty)
+				if (qty > 0):
+					items.new(item[0], item[1], qty)
+
 func interact():
 	say(discussion)
 	
--- zones/stations/STA1/waiter.gd
+++ zones/stations/STA1/waiter.gd
@@ -5,5 +5,13 @@ func _init():
 		["Hello.", [
 			["Hey, what's on the menu?.", _trade],
 			["Bye.", _end]
-		]]
+		]],
+		[
+			[ Item.ItemType.ITEM_QUEST, "access_card_1"],
+			[ Item.ItemType.ITEM_MISCELLANEOUS, "credit", 10],
+			[ Item.ItemType.ITEM_CONSUMABLES, "orange_1", 16],
+			[ Item.ItemType.ITEM_CONSUMABLES, "ham_sandwich_1", 5],
+			[ Item.ItemType.ITEM_CONSUMABLES, "pizza_1", 4],
+			[ Item.ItemType.ITEM_CONSUMABLES, "soft_drink_1", 10],
+		], 10
 	)
